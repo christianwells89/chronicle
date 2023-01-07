b22c4356db9ebe7885a867d7975771da295f3030
Christian <christianwells89@outlook.com>
2023-01-07 16:47:39 -0500

    chore: bump next version

--- components/link.tsx
+++ components/link.tsx
@@ -1,14 +1,19 @@
-import { chakra } from '@chakra-ui/react';
+import { Link as ChakraLink } from '@chakra-ui/react';
 import NextLink from 'next/link';
 import type { ComponentProps } from 'react';
 
-type LinkProps = { href: string } & ComponentProps<typeof chakra.a>;
+type LinkProps = { href: string } & ComponentProps<typeof ChakraLink>;
 
 export const Link: React.FC<LinkProps> = ({ href, children, ...props }) => (
-  <NextLink href={href} passHref>
-    {/* eslint-disable-next-line react/jsx-props-no-spreading */}
-    <chakra.a cursor="pointer" height="fit-content" {...props}>
-      {children}
-    </chakra.a>
-  </NextLink>
+  <ChakraLink
+    as={NextLink}
+    href={href}
+    // The only links so far are buttons/cards so the underlining is not necessary. When there are
+    // actual text links needed this component should be split up
+    _hover={{ textDecoration: 'none' }}
+    // eslint-disable-next-line react/jsx-props-no-spreading
+    {...props}
+  >
+    {children}
+  </ChakraLink>
 );
